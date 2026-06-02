882f365ce3757f387ed269d387d5a9d70f7a3f6d
sophiepavlova <olni.pavlova@gmail.com>
2026-06-02 13:11:37 -0700

    fix the Box component

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ const average = (arr) =>
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
   return (
     <>
       <NavBar>
@@ -61,6 +62,10 @@ export default function App() {
       <Main>
         <Box>
           <MovieList movies={movies} />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
         </Box>
       </Main>
     </>
