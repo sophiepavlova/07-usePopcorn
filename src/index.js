c2e3c1928bd88b378fcbea6172083b34d1a3c743
sophiepavlova <olni.pavlova@gmail.com>
2026-06-05 15:32:10 -0700

    Finish the StarRating component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 // import './index.css';
 // import App from './App';
 
 import StarRating from './StarRating';
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      {' '}
+      <StarRating
+        color='blue'
+        maxRating={9}
+        onSetMovieRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={9} />
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Ok', 'Good', 'Lovely!']}
+    />
+    <StarRating color='purple' size={24} className='test' />
+    <StarRating
+      maxRating={3}
+      color='red'
+      size={100}
+      messages={['Great job!', 'Marvelous', 'Splendid!']}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>,
 );
